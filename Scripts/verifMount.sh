50843961660be502c806a5cab27e3f09c92540d2
Emmanuel Gavalda <>
2019-05-18 18:26:40 +0200

    add files for wiki

---
+++ Scripts/verifMount.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -e /home/pi/liveboxShare/jesuisla ]
+then
+	# envoie de Message
+	echo "successful Mount SMB for Cam" | /home/pi/Scripts/Telegram/tg.sh
+else
+	# envoie de Message
+	echo "Failed Mount SMS for Cam" | /home/pi/Scripts/Telegram/tg.sh
+	sudo mount -a
+fi
